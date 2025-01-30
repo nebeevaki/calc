fd0905c9721027e80284c19ab6c17d30cb911f1d
nebeevaki <beevakirm@gmail.com>
2025-01-30 11:58:31 +0300

    added stack and precedence

---
+++ main.py
@@ -0,0 +1,43 @@
+def main():
+    s = input().split()
+    stack(s)
+
+
+def stack(s):
+    st = []
+    re = []
+
+    for i in s:
+        if i.isdigit():
+            re.append(i)
+        elif i == "(":
+            st.append(i)
+        elif i == ")":
+            while st and st[-1] != "(":
+                re.append(st.pop())
+            st.pop()
+        else:
+            while st and precedence(st[-1]) >= precedence(i):
+                re.append(st.pop())
+            st.append(i)
+
+
+    for i in range(len(st) - 1, -1, -1):
+        re.append(st.pop(i))
+
+    print(*re)
+
+def precedence(op):
+    if op == '+' or op == '-':
+        return 1
+    if op == '*' or op == '/':
+        return 2
+    if op == '^':
+        return 3
+    return 0
+
+
+
+if __name__ == '__main__':
+    main()
+
